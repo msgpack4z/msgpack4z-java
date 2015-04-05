7f6f5e25bb325e72f67c4e6cfbe21726cc6ebd4e
kenji yoshida <6b656e6a69@gmail.com>
2015-04-06 01:53:45 +0900

    avoid unused warnings in REPL

--- project/Build.scala
+++ project/Build.scala
@@ -57,6 +57,9 @@ object build extends Build {
       "-language:existentials" ::
       "-language:higherKinds" ::
       "-language:implicitConversions" ::
+      Nil
+    ),
+    scalacOptions in compile ++= (
       "-Ywarn-unused" ::
       "-Ywarn-unused-import" ::
       Nil
