72ebd6909af0e74e858248e3226bd7830c71924f
xuwei-k <6b656e6a69@gmail.com>
2016-01-22 19:28:36 +0900

    "org.msgpack" % "msgpack-core" % "0.8.2"

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,7 @@ object build extends Build {
     javacOptions in (Compile, doc) ++= Seq("-locale", "en_US"),
     commands += Command.command("updateReadme")(UpdateReadme.updateReadmeTask),
     libraryDependencies ++= (
-      ("org.msgpack" % "msgpack-core" % "0.8.1") ::
+      ("org.msgpack" % "msgpack-core" % "0.8.2") ::
       ("com.github.xuwei-k" % "msgpack4z-api" % "0.2.0") ::
       Nil
     ),
